439d2cffad262e1a39137fa622aacd486f847ad2
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-11-09 14:51:35 +0100

    en locale

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Deps from "./deps";
 import UI from "./ui";
+import en from "./locales/en";
 
 const connect = connector => {
   Deps.Env = connector.Env;
-  Deps.I18n = connector.I18n;
   Deps.Utils = connector.Utils;
+  Deps.I18n = connector.I18n;
+  Deps.I18n.en = { ...Deps.I18n.en, ...en };
 };
 
-export { Deps, UI, connect };
+export { UI, connect };
---
+++ src/locales/en.js
@@ -0,0 +1,14 @@
+const en = {
+  ui: {
+    form: {
+      sending: "Sending...",
+      success: "Success",
+      errors: {
+        connection: "Connection Error",
+        invalid_data: "Invalid data"
+      }
+    }
+  }
+};
+
+export default en;
