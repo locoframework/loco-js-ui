9321bff6ff3a8b335bd567c481a0c3e0188d496c
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2020-02-28 01:34:26 +0100

    [test]

---
+++ __tests__/deps.test.js
@@ -0,0 +1,42 @@
+import Deps from "deps.js";
+import { connect } from "index";
+
+const connectorThatOverwrites = {
+  Env: { loco: null },
+  I18n: {
+    en: {
+      ui: {
+        form: {
+          sending: "requesting"
+        }
+      }
+    }
+  }
+};
+
+const connectorThatAdds = {
+  Env: { loco: null },
+  I18n: {
+    pl: {
+      ui: {
+        form: {
+          sending: "wysyłam..."
+        }
+      }
+    }
+  }
+};
+
+describe("I18n", () => {
+  it('can be overridden', () => {
+    connect(connectorThatOverwrites);
+    expect(Object.keys(Deps.I18n.en.ui.form).length).toEqual(1);
+    expect(Deps.I18n.en.ui.form.sending).toEqual("requesting");
+  });
+
+  it('can be enhanced', () => {
+    connect(connectorThatAdds);
+    expect(Object.keys(Deps.I18n.en.ui.form).length).toEqual(3);
+    expect(Deps.I18n.pl.ui.form.sending).toEqual("wysyłam...");
+  });
+});
